a599590bebb0d9d1e6ec733fec313db89e77ac16
Leodgar2010 <leodgar2010@yandex.ru>
2022-11-24 19:40:26 +0300

    Домашнее задание к первому семинару

---
+++ Homework/src/homework2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main(String[] args) {
+        //Вывести все простые числа от 1 до 1000
+        int resultsimple = 0;
+        int temp = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        resultsimple = input.nextInt();
+        input.close();
+        //System.out.println("1");
+        for (int i = 2; i < resultsimple + 1; i++) {
+            for (int j = 2; j < i; j++) {
+                //System.out.println(i%j);
+                if ((i % j) == 0) {
+                    temp = temp + 1;
+                }
+            }
+            if (temp == 0) {
+                System.out.print(i+" ");
+            } else {
+                temp = 0;
+            }
+        }
+    }
+}
+
